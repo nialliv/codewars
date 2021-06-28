0224f9729da292c4ef32d757d476aaefdc16a55c
nialliv <artemjevnikita@ya.ru>
2021-06-29 00:51:33 +0400

    add solution

--- creditCardMask.js
+++ creditCardMask.js
@@ -1,3 +1,15 @@
 function maskify(cc) {
-    
+    let res = '';
+    for (let i = 0; i < cc.length - 4; i++){
+        res += "#";
+    }
+    return res + cc.slice(-4);
 }
+
+// Решение которое лучше этого...
+// function maskify(cc) {
+//     return cc.slice(0, -4).replace(/./g, '#') + cc.slice(-4);
+// }
+
+console.log("123456789");
+console.log(maskify("123456789"));
