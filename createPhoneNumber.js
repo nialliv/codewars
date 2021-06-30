62484bbc902d5bc3ac71950688caec38940bff0f
nialliv <artemjevnikita@ya.ru>
2021-06-30 23:09:31 +0400

    add PhoneNumber and update gitignore

---
+++ createPhoneNumber.js
@@ -0,0 +1,11 @@
+// Не особо заморачиваясь можно выполнить это так...
+
+function createPhoneNumber(numbers) {
+    return `(${numbers.slice(0, 3).join('')}) ${numbers.slice(3,6).join('')}-${numbers.slice(-4).join('')}`
+}
+
+// Решение которое кажется более интересным
+
+function createPhoneNumber(numbers){
+    return numbers.reduce((p,c) => p.replace('x',c), "(xxx) xxx-xxxx");
+}
